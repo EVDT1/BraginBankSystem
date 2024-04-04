c277233c25654878b9bed1954fecb4b37a359464
bragin.ed <braginevgenij1103@gmail.com>
2024-04-04 17:21:15 +0300

    econd Commit

--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,4 +1,35 @@
 package data;
-
 public class Client {
+    private String name;
+    private String surname;
+    private String card;
+
+    public Client(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCard() {
+        return card;
+    }
 }
